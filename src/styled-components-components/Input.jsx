0d0837de871f932f6fcf7538136d13cb77e1a5c0
ArcanisCz <arcaniscz@gmail.com>
2022-02-01 18:55:20 +0100

    styled components 2

--- src/styled-components-components/Input.jsx
+++ src/styled-components-components/Input.jsx
@@ -5,6 +5,10 @@ export const Input = ({text = "input text", color = "#fffa12"}) => {
   return <StyledInput value={text} borderColor={color} />
 }
 
-const StyledInput = styled.input`
+const StyledInput = styled.input.attrs((props) => ({
+  style: {
+    color: "blue",
+  },
+}))`
   border: ${({borderColor}) => `2px solid ${borderColor}`};
 `;
