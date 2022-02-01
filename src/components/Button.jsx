4c96c58b0cbf262cf20c2f6b81350200f21121a3
ArcanisCz <arcaniscz@gmail.com>
2022-02-01 17:06:20 +0100

    styled components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import styled from "styled-components";
 import styles from "./Button.module.css";
 
+// export const Button = () => (
+//     <button className={styles.pokus}>aaa</button>
+// )
+
 export const Button = () => (
-    <button className={styles.pokus}>aaa</button>
+  <StyledButton >aaa</StyledButton>
 )
+
+const StyledButton = styled.button`
+  color: palevioletred;
+`;
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import styled from "styled-components";
 import {Button} from "./components/Button";
 import './style.css';
 
@@ -7,7 +8,7 @@ const App = () => (
   <div className="container">
     <header>
       <div className="logo"></div>
-      <h1>Webová aplikace</h1>
+      <Title>Webová aplikace</Title>
     </header>
     <main>
       <p>Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>.</p>
@@ -18,5 +19,11 @@ const App = () => (
     </footer>
   </div>
 );
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 render(<App />, document.querySelector('#app'));
